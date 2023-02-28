567940d85297e7ce94363d829f98d74e12c625d2
Quan Van <vanthuanquan@gmail.com>
2023-02-28 16:56:18 +0700

    update:

---
+++ src/app/demo_tutorial/Ground.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Ground = () => {
+  return (
+    <mesh rotation-x={Math.PI * -0.5} receiveShadow>
+      <planeGeometry args={[1000, 1000]} />
+      <meshStandardMaterial color="#458754" />
+    </mesh>
+  )
+}
+
+export default Ground
